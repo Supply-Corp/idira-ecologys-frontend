ff6d9350334da202a8ba1e7099895c1b716352b1
DouglasCollura <douglascollura@gmail.com>
2024-03-04 04:44:57 -0400

    configure menu data

---
+++ src/app/shared/data/menu.data.ts
@@ -0,0 +1,81 @@
+import {  MenuRoute } from "@interfaces/menu"
+import { Roles } from "@interfaces/roles"
+
+  const AnunciosRoute: MenuRoute = {
+    title:'Anuncios',
+    icon:'speed',
+    url:'/home',
+    extact:true
+  };
+
+  const CategoriasRoute: MenuRoute = {
+    title:'Categorías',
+    icon:'category',
+    url:'/home/categories',
+    extact:false
+  };
+
+  const EmpresasRoute: MenuRoute = {
+    title:'Empresas',
+    icon:'factory',
+    url:'/home/companies',
+    extact:false
+  }
+
+  const UsuariosRoute: MenuRoute = {
+    title:'Usuarios',
+    icon:'group',
+    url:'/home/users',
+    extact:false
+  }
+
+  const DocumentosRoute: MenuRoute = {
+    title:'Documentos',
+    icon:'folder',
+    url:'/home/documents',
+    extact:false
+  }
+
+  const ActividadesMensualesRoute: MenuRoute = {
+    title:'Actividades Mensuales',
+    icon:'auto_stories',
+    url:'/home/activities',
+    extact:false
+  }
+
+  const PerfilRoute: MenuRoute = {
+    title:'Datos de Perfil',
+    icon:'person',
+    url:'/home/profile',
+    extact:false
+  }
+
+  const AdminRoutes:MenuRoute[] = [
+    AnunciosRoute,
+    CategoriasRoute,
+    EmpresasRoute,
+    UsuariosRoute,
+    DocumentosRoute,
+    ActividadesMensualesRoute,
+    PerfilRoute
+  ];
+
+  const CompanyRoutes:MenuRoute[] = [
+    AnunciosRoute,
+    EmpresasRoute,
+    PerfilRoute
+  ];
+
+  const SedeRoutes:MenuRoute[] = [
+    AnunciosRoute,
+    EmpresasRoute,
+    DocumentosRoute,
+    ActividadesMensualesRoute,
+    PerfilRoute
+  ];
+
+  export const menuRoute ={
+    [Roles.ADMIN]:AdminRoutes,
+    [Roles.USER]:CompanyRoutes,
+    [Roles.SEDE]:SedeRoutes
+  }
--- src/app/shared/layout/menu/menu.component.ts
+++ src/app/shared/layout/menu/menu.component.ts
@@ -4,6 +4,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
 import { Router, RouterModule } from '@angular/router';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import { menuRoute } from '@shared/data/menu.data';
+import { MenuRoute } from '@interfaces/menu';
 
 
 @Component({
@@ -23,6 +25,7 @@ import {MatTooltipModule} from '@angular/material/tooltip';
 export class MenuComponent {
 
   private router = inject(Router);
+  public menuList:MenuRoute[] = menuRoute.ADMIN;
 
 
   goTo(url:string){
